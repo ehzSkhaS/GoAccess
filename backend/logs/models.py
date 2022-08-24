78e0eee0e4649392e56573e64480345be2136058
ehzSkhaS <ehz.skhas@gmail.com>
2022-08-24 12:24:08 -0400

    Log System in Progress

--- backend/logs/models.py
+++ backend/logs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Alert(models.Model):
+    description = models.CharField(max_length=255, null=False)
+    created_date = models.DateTimeField(default=timezone.now, editable=False)
+    class Meta:
+        db_table = 'alert'
+        verbose_name = "Alert"
+        verbose_name_plural = "Alerts"
+        
+    def __str__(self):
+        return str(self.description)
+
