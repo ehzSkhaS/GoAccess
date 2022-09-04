ac56071adb83bf14052c3699fb2eeb38e56ba69d
Erick Salinas Gallego <you@example.com>
2022-09-03 23:36:03 -0700

    Set admin entries, serializers, views and urls to models RouteSuperArea, RouteArea, Route, Checkpoint, Round, RouteRound.

--- backend/control/serializers.py
+++ backend/control/serializers.py
@@ -2,11 +2,6 @@ from rest_framework import serializers
 
 from .models import *
 
-class AreaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Area
-        fields = '__all__'
-
 
 class RouteSuperAreaSerializer(serializers.ModelSerializer):
     class Meta:
